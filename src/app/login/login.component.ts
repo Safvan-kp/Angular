b743551fe9697d765d9048c78ed3d0961f5f9972
Safvan-kp <safvan.kp@gadgeon.com>
2022-10-15 17:06:03 +0530

    second

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  loginForm!:FormGroup
+  constructor(private formbuilder:FormBuilder,private http:HttpClient,private router:Router) {
+    
+   }
 
   ngOnInit(): void {
+    this.loginForm=this.formbuilder.group({
+      email:[''],
+      password:['']
+    })
+
+  }
+  login(){
+
+    this.http.get<any>('http://localhost:3000/user').subscribe(abc=>{
+      
+      const user=abc.find((a:any)=>{
+        return a.email===this.loginForm.value.email&&a.password===this.loginForm.value.password
+      }
+      )
+      if(user){
+        alert("login successs" );
+        this.loginForm.reset;
+      }
+      else{
+        alert("NOPE!!!!!!!!!!!");
+      }
+      }
+     
+    );
   }
 
 }
